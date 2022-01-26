5f0b95df9bd014d7ed6bff291c069151ee433a63
Seth Zhao <sethzhao506@berkeley.edu>
2022-01-26 14:23:06 -0800

    update Map by unique index

--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,36 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-
+use std::collections::HashMap;
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map, Index, IndexList, IndexedMap, Item, MultiIndex, UniqueIndex};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
-    pub score: i32,
-    pub owner: Addr,
+  pub owner: Addr,
+  pub score: i32,
+  pub identifier: u8, // <---- unique value
+}
+
+// TokenIndexes structs keeps a list of indexers
+pub struct StateIndexes<'a> {
+  // token.identifier
+  pub identifier: UniqueIndex<'a, U8Key, State>,
+}
+
+// IndexList is just boilerplate code for fetching a struct's indexes
+impl<'a> IndexList<State> for StateIndexes<'a> {
+  fn get_indexes(&'_ self) -> Box<dyn Iterator<Item=&'_ dyn Index<State>> + '_> {
+    let v: Vec<&dyn Index<State>> = vec![&self.identifier];
+    Box::new(v.into_iter())
+  }
+}
+
+// storage access function.
+pub fn states<'a>() -> IndexedMap<'a, &'a [u8], State, StateIndexes<'a>> {
+  let indexes = StateIndexes {
+    identifier: UniqueIndex::new(|d| U8Key::new(d.identifier), "state_identifier"),
+  };
+  IndexedMap::new(TOKEN_NAMESPACE, indexes)
 }
 
 pub const STATE: Item<State> = Item::new("state");
